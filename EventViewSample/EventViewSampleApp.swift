1c702709a805b32ecd33dad49f4dcd3bf720b754
Kame2021 <21cm0108@jec.ac.jp>
2022-12-19 13:41:44 +0900

    Initial Commit

---
+++ EventViewSample/EventViewSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventViewSampleApp.swift
+//  EventViewSample
+//
+//  Created by Ryo on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct EventViewSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
